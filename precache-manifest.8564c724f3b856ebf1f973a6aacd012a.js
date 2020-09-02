3b2e0cd30a8e68b3469a2e9c8f7e5c411af260c8
James Burbage <jamesburbage@MacBook-Pro.local>
2020-09-02 12:12:34 -0600

    Updates

---
+++ precache-manifest.8564c724f3b856ebf1f973a6aacd012a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e22e16af1756f4dc2f91c61f7c628a2c",
+    "url": "/seasons_react/index.html"
+  },
+  {
+    "revision": "76ac2088ea1d7e274c44",
+    "url": "/seasons_react/static/css/main.c5c5e061.chunk.css"
+  },
+  {
+    "revision": "c2d246ecc2b83ccac6a6",
+    "url": "/seasons_react/static/js/2.8b77c9df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/seasons_react/static/js/2.8b77c9df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76ac2088ea1d7e274c44",
+    "url": "/seasons_react/static/js/main.dc345ec0.chunk.js"
+  },
+  {
+    "revision": "7d86cc5ad0cd6acc0440",
+    "url": "/seasons_react/static/js/runtime-main.2234296b.js"
+  }
+]);
